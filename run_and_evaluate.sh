cb42f98c7e5f25eaaff9672aae068157dafe8d30
Prakruti <prakruti.gogia@gmail.com>
2017-12-14 13:08:19 -0500

    Added some more context for the entire pipeline

--- run_and_evaluate.sh
+++ run_and_evaluate.sh
@@ -5,6 +5,14 @@
 InfiniTAM_executable_path="/Users/prakrutigogia/Documents/CMU/Academics/Fall_17/Geometry/Project/InfiniTAM/InfiniTAM/build/Apps/InfiniTAM_cli"
 executable_name='./InfiniTAM_cli'
 
+
+#Note run 
+#mogrify -format ppm frame-000*.color.png
+#mogrify -format pgm frame-000*.depth.png
+#modify the calibration file - TODO write a python script to automatically generate a calib.txt in the right format
+
+#
+
 #change to folder containing executable
 cd $InfiniTAM_executable_path
 
@@ -12,7 +20,7 @@ calib_filename='calib.txt'
 rgb_filename='frame-%06i.color.ppm'
 depth_filename='frame-%06i.depth.pgm'
 
-#modify this to add the 
+#modify this to add whichever datasets you're using for evaluation
 declare -a dataset_name_array=("minion_data/" "umbrella_data/")
 
 #Specify path to the folder containing the datasets (absolute path) the mesh will be stored here
@@ -27,7 +35,14 @@ do
 	rgbfile=$dataset_path$rgb_filename
 
 	depthfile=$dataset_path$depth_filename
-
+	
+	echo "*******Started running ..**********" 
+	echo $dataset_name
 	$executable_name $calibfile $rgbfile $depthfile $dataset_path
+	echo "*******Finished running ..**********" 
+
+	#TODO : convert all .stl files to .ply
+	
+
 done
 
